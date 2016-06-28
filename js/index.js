384b1e056d9ca549cd22d178c7214c6ca8d1d1f4
Allison Paul <allison.paul221@gmail.com>
2016-06-28 13:49:49 -0500

    Add Jquery for social icons

---
+++ js/index.js
@@ -0,0 +1,8 @@
+ $(document).ready(function(){
+    $('.social-icon').hover(function() {
+        $(this).addClass('transition');
+ 
+    }, function() {
+        $(this).removeClass('transition');
+    });
+  });
